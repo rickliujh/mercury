4d18af651ceb26aa30810bd901f0d1d81d3d6fd1
rick liu <rick.ryan2@outlook.com>
2025-03-01 23:57:08 +0000

    feat: simplest memory allocation

--- common.h
+++ common.h
@@ -40,7 +40,8 @@ typedef uint32_t vaddr_t;
 #define va_end __builtin_va_end
 // va_end: This macro cleans up the va_list variable. It must be called after
 // you have finished accessing the additional arguments.
-#define va_arg __builtin_va_arg
+#define va_arg    __builtin_va_arg
+#define PAGE_SIZE 4096
 
 void *memset(void *buf, char c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
