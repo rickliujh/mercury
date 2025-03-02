8e834117f1d7722e409fe0f9f28f1559288265b1
rick liu <rick.ryan2@outlook.com>
2025-03-02 00:06:19 +0000

    feat: comment for alloc_pages

--- kernel.c
+++ kernel.c
@@ -142,15 +142,19 @@ extern char __free_ram[], __free_ram_end[];
 
 paddr_t
 alloc_pages(uint32_t n) {
-    static paddr_t next_paddr = (paddr_t) __free_ram;
+    // static variable with initialization, this part is executed only once—when
+    // the program starts (or when the variable is first defined in some
+    // systems). After that, next_paddr keeps its value from the last function
+    // call because of its static nature
+    static paddr_t next_paddr = (paddr_t)__free_ram;
     paddr_t paddr = next_paddr;
     next_paddr += n * PAGE_SIZE;
 
-    if (next_paddr > (paddr_t) __free_ram_end) {
+    if (next_paddr > (paddr_t)__free_ram_end) {
         PANIC("out of memory");
     }
 
-    memset((void *) paddr, 0, n * PAGE_SIZE);
+    memset((void *)paddr, 0, n * PAGE_SIZE);
     return paddr;
 }
 
